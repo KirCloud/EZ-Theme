e25a11e5509fbccf126f63693ab34faf6dac79c5
KirCloud <li1322958826@gmail.com>
2025-11-22 03:37:03 +0800

    1.修正格式

--- src/views/docs/DocDetail.vue
+++ src/views/docs/DocDetail.vue
@@ -706,7 +706,8 @@ const buildNoAccessCardHtml = () => {
               <span>${buyText}</span>
             </button>
             <button class="action-button secondary" data-href="/tickets">
-            <svg data-v-6764ce0f="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" size="16" class="tabler-icon tabler-icon-message btn-icon"><path d="M8 9h8"></path><path d="M8 13h6"></path><path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z"></path></svg>              <span>${supportText}</span>
+            <svg data-v-6764ce0f="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" size="16" class="tabler-icon tabler-icon-message btn-icon"><path d="M8 9h8"></path><path d="M8 13h6"></path><path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z"></path></svg>
+            <span>${supportText}</span>
             </button>
           </div>
         </div>
